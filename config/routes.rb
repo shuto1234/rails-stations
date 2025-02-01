cd0cebb8634f56d21e13dfcbc74d1379c9f45efc
shuto1234 <171531334+shuto1234@users.noreply.github.com>
2025-02-01 10:37:09 +0900

    station2クリア

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'movies/index'
+  root to: 'movies#index'
   get 'movies/show'
+
+  get 'admin/movies' => 'admin/movies#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +12,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  root to: 'movies#index'
 end
