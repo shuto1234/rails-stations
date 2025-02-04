18cf4a7e786eb7ee4cd02a66aa0388c83e1bb538
shuto1234 <171531334+shuto1234@users.noreply.github.com>
2025-02-04 16:11:38 +0900

    station3クリア

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,26 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  def new
+    @movie = Movie.new
+  end
+  def create
+    # 1.データを新規登録するためのインスタンス変数作成
+    @movie = Movie.new(movie_params)
+    # 2.データをデータベースに保存するためのsaveメソッド
+    if @movie.save
+      # 3.任意の  画面へリダイレクト
+      redirect_to admin_movies_path
+    else
+      # 4.saveできなかったときに、admin::movies_controllerのnewのviewに飛ぶ
+      render :new
+    end
+  end
+
+  private
+  
+  # ストロングパラメータ
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  end
 end
